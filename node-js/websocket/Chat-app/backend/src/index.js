df2292b7282dd3608cadc114d0df5449be962f0d
Riddhi Chavan <riddhic164@gmail.com>
2026-02-01 01:18:01 +0530

    fix: update CORS configuration to use environment variable for frontend URL

--- node-js/websocket/Chat-app/backend/src/index.js
+++ node-js/websocket/Chat-app/backend/src/index.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(cookieparser())
 app.use(
     cors({
-        origin : ["http://localhost:5173"],
+        origin: process.env.FRONTEND_URL || "http://localhost:5173",
         credentials : true
     })
 )
@@ -30,4 +30,3 @@ server.listen(PORT , () => {
     console.log("Server is running on port" , PORT)
     connectDB()
 })
-
